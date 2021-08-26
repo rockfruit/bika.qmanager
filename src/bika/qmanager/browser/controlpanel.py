721dfe4af7165d4356ec38d200f3727ec8bd3a69
Lunga Baliwe <lunga001@gmail.com>
2021-08-26 16:15:06 +0200

    Extend IQueueControlPanel from senaite.queue and add extra fields

---
+++ src/bika/qmanager/browser/controlpanel.py
@@ -0,0 +1,42 @@
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.z3cform import layout
+from senaite.queue import messageFactory as _
+from senaite.queue.browser.controlpanel import IQueueControlPanel
+from senaite.queue.browser.controlpanel import QueueControlPanelForm
+from zope import schema
+from plone.supermodel import model
+
+
+class IBikaQueueControlPanel(IQueueControlPanel):
+    """Control panel Settings
+    """
+    model.fieldset(
+        'synchronous_processing_cap',
+        label=_("Synchronous processing cap"),
+        fields=['samples_analyses', 'worksheet_analyses', 'coa_publication']
+    )
+
+    samples_analyses = schema.Int(
+            title=_("Samples' Analyses"),
+            description=_("The nyaka limits are specified for the number of Samples Analyses"),
+            required=False,
+        )
+
+    worksheet_analyses = schema.Int(
+            title=_("Worksheet Analyses"),
+            description=_("The limits are specified for the number of Worksheet Analyses"),
+            required=False,
+        )
+    coa_publication = schema.Bool(
+            title=_("COA Publication"),
+            description=_("LIMS includes the Publication transition in asynchronous processing"),
+            required=False,
+        )
+
+class BikaQueueControlPanelForm(QueueControlPanelForm):
+    schema = IBikaQueueControlPanel
+    schema_prefix = "senaite.queue"
+
+
+BikaQueueControlPanelView = layout.wrap_form(BikaQueueControlPanelForm,
+                                         ControlPanelFormWrapper)
