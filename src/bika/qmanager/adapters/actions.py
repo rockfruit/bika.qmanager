7cd7f3a0ac2edbd52cbc74d79ecc224cc129d936
Lunga Baliwe <lunga001@gmail.com>
2021-09-01 22:52:09 +0200

    Add transition receive on samples folder

--- src/bika/qmanager/adapters/actions.py
+++ src/bika/qmanager/adapters/actions.py
@@ -11,16 +11,23 @@ class WorkflowActionGenericQueueAdapter(WorkflowActionGenericAdapter):
 
     def do_action(self, action, objects):
 
-        worksheet_analyses = ploneapi.portal.get_registry_record('senaite.queue.worksheet_analyses')
-        if worksheet_analyses > len(objects):
+        do_queue = True
+        # samples folder
+        if self.context.portal_type == 'Samples':
+            samples_analyses = ploneapi.portal.get_registry_record('senaite.queue.samples_analyses')
+            if samples_analyses > len(objects):
+                do_queue = False
+
+        # worksheets
+        if self.context.portal_type == 'Worksheet':
+            worksheet_analyses = ploneapi.portal.get_registry_record('senaite.queue.worksheet_analyses')
+            if worksheet_analyses > len(objects):
+                do_queue = False
+        if not do_queue:
             return super(WorkflowActionGenericQueueAdapter, self).do_action(
                 action, objects)
 
-        # Check context: if context is worksheet
-
         # samples_analyses, worksheet_analyses, coa_publication
-        
-
         # Delegate to base do_action
         # check here
         if api.is_queue_ready(action):
---
+++ src/bika/qmanager/browser/viewlets/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import itertools
+from plone.app.layout.viewlets import ViewletBase
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from senaite.queue import api
+
+
+class QueuedSamplesSampleViewlet(ViewletBase):
+    """Prints a viewlet to display a message stating there are some analyses
+    that are in queue to be assigned to a worksheet
+    """
+    index = ViewPageTemplateFile("templates/queued_samples_sample_viewlet.pt")
+
+    def __init__(self, context, request, view, manager=None):
+        super(QueuedSamplesSampleViewlet, self).__init__(
+            context, request, view, manager=manager)
+        self.context = context
+        self.request = request
+        self.view = view
+
+    def get_num_analyses_pending(self):
+        if not api.is_queue_enabled():
+            return 0
+
+        # We are only interested in tasks with uids
+        queue = api.get_queue()
+        uids = map(lambda t: t.get("uids"), queue.get_tasks_for(self.context))
+        uids = filter(None, list(itertools.chain.from_iterable(uids)))
+        return len(set(uids))
